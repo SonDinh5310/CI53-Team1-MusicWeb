b01fa68485c7c0e6401efe81065ba02e00cadb36
Sondinh <dnson20022001@gmail.com>
2021-01-08 19:00:57 +0700

    commit code

--- js/components/navbarContainer.js
+++ js/components/navbarContainer.js
@@ -2,6 +2,9 @@ const $template = document.createElement("template");
 
 $template.innerHTML = /*html */ `
     <style>
+        * {
+            font-family: "Roboto Mono", monospace;
+        }
         ul {
             list-style: none;
             display: flex;
@@ -24,29 +27,61 @@ $template.innerHTML = /*html */ `
             color: #feffff;
             font-style: none;
             text-decoration: none;
-            font-family: "Lobster";
+            font-family: Lobster;
             font-size: 48px;
+            cursor: pointer;
         }
-        #search-bar {
-            width: 400px;
+        #page-logo:hover {
+            text-shadow: 0 0 11px #17252a;
+        }
+        #input-bar {
+            width: 300px;
             height: 30px;
             border: none;
             border-radius: 20px;
             outline: none;
             padding: auto;
         }
+        #search-bar {
+            background-color: #feffff;
+            border-radius: 20px;
+            padding-left: 8px;
+            padding-right: 8px;
+        }
+        #search-bar > button {
+            font-size: 16px;
+            border-radius: 20px;
+            outline: none;
+            background-color: transparent;
+            color: #3aafa9;
+            border: none;
+            cursor: pointer;
+        }
+        
+        #get-started-btn {
+            width: 100px;
+            height: 30px;
+            border-radius: 20px;
+            outline: none;
+            border: none;
+            cursor: pointer;
+            font-size: 12px;
+        }
+        #get-started-btn:hover {
+            box-shadow: 0 0 11px #17252a; 
+        }
     </style>
     <nav id="page-nav" class="fixed-nav-bar">
         <ul>
             <li><a href="#" id="page-logo">.Music</a></li>
             <li>
-            <form>
-                <input type="text" placeholder="Write something ..." id="search-bar">
-                <button>Seatch</button>
+            <form id="search-bar">
+                <input type="text" placeholder="Write something ..." id="input-bar">
+                <button>Search</button>
             </form>
             </li>
             <li>
-                <button>Get Started</button>
+                <button id="get-started-btn">Get Started</button>
             </li>
         </ul>
     </nav>
